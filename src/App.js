9f3918a5d83f9ec93aa366d184a758e066546546
ronaldodeveloper <rgm.frontend@hotmail.com>
2021-08-25 15:22:11 -0300

    update: componente que exibe os videos criados

--- src/App.js
+++ src/App.js
@@ -37,6 +37,47 @@ function App() {
    }
    console.log(filterSearch)
 
+
+  // TEXT result is
+  const resultados= [
+    {
+    id:1,   
+    titulo: 'Titulo do Video',
+    descricao: 'video sobre?',
+    thumb: 'avatar'
+    },
+    {
+      id:2,   
+      titulo: 'Titulo do Video 2',
+      descricao: 'video sobre',
+      thumb: 'avatar 2'
+    },
+    {
+      id:3,   
+      titulo: 'Titulo do Video 3',
+      descricao: 'video sobre',
+      thumb: 'avatar 3'
+    },
+    {
+      id:4,   
+      titulo: 'Titulo do Video 4',
+      descricao: 'video sobre',
+      thumb: 'avatar 4'
+    },
+    {
+      id:5,   
+      titulo: 'Titulo do Video 5',
+      descricao: 'video sobre',
+      thumb: 'avatar 5'
+    }
+  ]
+
+
+ const [play, setPlay]= useState()
+ const Player= ()=>{
+   setPlay('Play')
+ }
+
   return (
     <main>
       <section className="searchContainer">
@@ -50,14 +91,32 @@ function App() {
         <button type="submit">search</button>
       </form>
       </section>
-      <section className="Container">
-        <article className="result">
 
+      <section className="content">
+        <article className="result">
+            <h1>Resultados</h1>
+            {resultados.map((el)=>{
+                   return (
+                     <section key={el.id} className="card" onClick={Player}>
+                          <div className="cardThumb"> 
+                             <h2>{el.thumb}</h2>
+                          </div>
+                          <div className="cardTitles"> 
+                            <h3>{el.titulo}</h3>
+                            <h4>{el.descricao}</h4>
+                          </div>
+                     </section>
+                   )
+            })}
+        
         </article>
-        <article className="videoPlay"></article>
+        <article className="videoPlay">
+          <h2>{play}</h2>
+        </article>
       </section>
     </main>
   );
 }
+
 
 export default App;
