ba89a1b34ec455f5ecb2e344eaef22fe478f0964
ronaldodeveloper <rgm.frontend@hotmail.com>
2021-08-25 12:15:11 -0300

    start: inicio do projeto

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  
+   const [seachVideo, setSearchVideo]= useState()
+   const [filterSearch, setFilterSearch]= useState()
+
+   useEffect(()=>{
+       apiYoutube()
+     }
+   ,[])
+
+   // API
+   const apiYoutube= async ()=> {
+    try{
+        const url= `https://www.googleapis.com/youtube/v3/search?part=id,snippet&q=react&maxresults=20&key=AIzaSyB2qPOWZw7J7GJ5rRjpL_tkfi4shZckQaE`;
+      
+        const response= await fetch(url)
+        const data = await response.json()
+        console.log(
+            JSON.parse(
+                JSON.stringify(
+                    Object.assign(data)
+        )))
+    }catch(error){
+        console.log(`API not connect!`)
+        console.log(error)
+      }   
+    }
+   // console.log(apiYoutube())
+  
+
+   const handleSubmit= (e)=>{ 
+     e.preventDefault(); 
+     const busca= { video: seachVideo};
+     setFilterSearch(busca.video)
+   }
+   console.log(filterSearch)
+
+  return (
+    <main>
+      <section className="searchContainer">
+      <form onSubmit={handleSubmit}>
+      <input 
+        type="search" 
+        name={seachVideo}
+        placeholder="Pesquisar vídeo"
+        onChange={ e=>setSearchVideo(e.target.value)} 
+        /> 
+        <button type="submit">search</button>
+      </form>
+      </section>
+      <section className="Container">
+        <article className="result">
+
+        </article>
+        <article className="videoPlay"></article>
+      </section>
+    </main>
+  );
+}
+
+export default App;
